2241adb08350142007bcc3b6526762401cafb712
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-09-02 09:58:48 -0400

    test email and generate

---
+++ Email_Daily_Ridership_Report.bat
@@ -0,0 +1,4 @@
+@echo off
+Powershell.exe -command "C:\Users\Michael.Nugent\Documents\Projects\R_Projects\nooj\Reporting\Daily\Daily_Ridership_Report\Send-Email_Ridership_test.ps1"
+
+
---
+++ Generate_Daily_Ridership_Report.bat
@@ -0,0 +1,4 @@
+cd "c:\Program Files\r\r-3.6.1\bin"
+
+rscript.exe C:\Users\Michael.Nugent\Documents\Projects\R_Projects\nooj\Reporting\Daily\Daily_Ridership_Report\Daily_Ridership_execute.r
+
