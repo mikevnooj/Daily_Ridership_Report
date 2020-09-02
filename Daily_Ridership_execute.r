64a363494125c760756d704cf3f38e78c573f1e5
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-09-02 09:57:39 -0400

    add blastula for later use, set env

--- Daily_Ridership_execute.r
+++ Daily_Ridership_execute.r
@@ -1,5 +1,20 @@
 # this renders the rmarkdown file for the daily ridership report.
 # This is the script that will be run in the windows tasks scheduler.
 
-rmarkdown::render(input = 'c:/users/scarter/downloads/R_Projects/Daily_ridership_reporting/Daily_Ridership.Rmd', 
+# library(blastula)
+
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
+
+rmarkdown::render(input = 'c:/users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Daily_Ridership_Report/Daily_Ridership.Rmd', 
                   output_file = paste0(format(Sys.Date(), "%Y%m%d"), " Preliminary Ridership Report",'.html')) 
+
+ 
+# x <- blastula::render_email(input = 'C:/Users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Daily_Ridership_Report/Daily_Ridership.Rmd')
+#  
+# x %>% smtp_send(
+#   to = c("michael.nugent@indygo.net","michaelvnugent@gmail.com")
+#   ,from = "michael.nugent@indygo.net"
+#   ,subject = "TEST"
+#   ,credentials = creds_key("outlook")
+#   )
+
