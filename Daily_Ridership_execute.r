8a5d5ab84f21ae7ddc4685e1348b8c36a632596d
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-09-03 16:17:20 -0400

    add output_file arg

--- Daily_Ridership_execute.r
+++ Daily_Ridership_execute.r
@@ -6,7 +6,7 @@
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
 
 rmarkdown::render(input = 'c:/users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Daily_Ridership_Report/Daily_Ridership.Rmd', 
-                  output_file = paste0(format(Sys.Date(), "%Y%m%d"), " Preliminary Ridership Report",'.html')) 
+                  output_file = paste0(format(Sys.Date(), "%Y%m%d"), " Preliminary Ridership Report",'.html'))
 
 
 # x <- blastula::render_email(input = 'C:/Users/Michael.Nugent/Documents/Projects/R_Projects/nooj/Reporting/Daily/Daily_Ridership_Report/Daily_Ridership.Rmd')
